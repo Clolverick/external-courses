ddb7abc9eac74b92990dfca2afd2d724eba31293
Clolverick <87826402+Clolverick@users.noreply.github.com>
2021-12-15 20:47:23 +0300

    ex16

---
+++ src/ex16_js-async/task-01.js
@@ -0,0 +1,26 @@
+function theFetch(url, params) {
+  return new Promise((resolve, reject) => {
+    console.log(url, params);
+
+    let method = 'GET';
+    if (params.method) {
+      method = params.method;
+    }
+
+    let body = [];
+    if (params.body) {
+      body = params.body;
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.onload = () => resolve(xhr.response);
+    xhr.onerror = () => reject(xhr.status);
+    xhr.ontimeout = () => {
+      console.log('503 Service Unavailable');
+    };
+    xhr.send(body);
+  });
+}
+
+module.export = theFetch;
